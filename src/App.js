ba64b057c357f7d9ce4ae9dad1c06e158e72f7de
aarondevtw <france7772001@icloud.com>
2023-03-10 03:56:40 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import './assets/all.scss';
+import Input from './components/Input';
 
 function App() {
+  const [text, setText] = useState('');
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
+  };
+  useEffect(() => {
+    const controller = new AbortController();
+    (async () => {
+      // console.log(process.env.REACT_APP_PATH);
+      try {
+        // 從環境變數取得api path
+        //
+        const path = process.env.REACT_APP_PATH;
+        const result = await axios.get(path, {
+          signal: controller.signal,
+        });
+        // const result = await axios.get('https://randomuser.me/api/');
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+
+    // clear funtion
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +50,17 @@ function App() {
         >
           Learn React
         </a>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+
+        {text}
+        <Input
+          id="sampleText"
+          text="這是一個input"
+          value={text}
+          onChangeHandler={onChangeHandler}
+        ></Input>
       </header>
     </div>
   );
